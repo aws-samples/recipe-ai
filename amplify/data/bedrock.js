c594d9d254e49f09989db82595e0bd4fcb57e896
olegchorny <oleg.chorny@gmail.com>
2024-10-01 16:21:15 +0300

    Update bedrock.js

--- amplify/data/bedrock.js
+++ amplify/data/bedrock.js
@@ -32,7 +32,7 @@ export function request(ctx) {
   
   const prompt = `Interpret this dream and provide actionable advice: ${ingredients.join(", ")}.`;
 
-  return {
+return {
     resourcePath: `/model/anthropic.claude-3-sonnet-20240229-v1:0/invoke`,
     method: "POST",
     params: {
@@ -42,14 +42,16 @@ export function request(ctx) {
       body: {
         anthropic_version: "bedrock-2023-05-31",
         max_tokens: 1000,
+        system: systemMessage,
         messages: [
           {
-            role: "system",
-            content: systemMessage
-          },
-          {
             role: "user",
-            content: prompt
+            content: [
+              {
+                type: "text",
+                text: `\n\nHuman:${prompt}\n\nAssistant:`,
+              },
+            ],
           },
         ],
       },
